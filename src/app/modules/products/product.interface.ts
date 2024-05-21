25fc4661f0041e9c2ae2bc110ae94187f5d20fc7
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2024-05-21 23:00:42 +0600

    create interface

---
+++ src/app/modules/products/product.interface.ts
@@ -0,0 +1,31 @@
+import { Schema, model, connect } from 'mongoose';
+
+export type Guardian ={
+    
+    fatherName:string;
+    fatherContactNo:string;
+    motherName:string;
+    motherContactNo:string;
+}
+export type UserName ={
+    
+    firstName:string;
+    middleName:string;
+    lastName:string;
+   
+}
+
+export type Student = {
+  id:string
+  name:UserName;
+  contactNo : string;
+  guardianNo:string;
+  gender: 'male'| 'female';
+  dateOfBirth : string;
+  email: string;
+  presentAddress:string;
+  permanentAddress:string;
+  bloodGroup?: '+A' | '+B' | '+AB' | '+O'|'-A' | '-B' | '-AB' | '-O';
+  guardian:Guardian;
+  isActive:'active'|'blocked'
+}
